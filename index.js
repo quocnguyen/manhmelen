83b8eaafdfe93f55fa53bb4e4e1e36d0020f342d
quocnguyen <quocnguyen@clgt.vn>
2016-07-05 02:10:07 +0700

    do not expose to the endpoint that we are using proxy

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 require('dotenv').config({silent: true});
 const http = require('http');
-const proxy = require('proxy');
+const proxy = require('./lib/proxy');
 const server = proxy(http.createServer());
 server.listen(process.env.PORT);
+
+console.log('proxy start on port', process.env.PORT);
